91c7cf9935259efd4f65b70079b859069454bc81
miyazaki <miyazaki@mdo.design>
2024-08-14 14:57:42 +0900

    js修正

--- app/public/wp-content/themes/Wooms_2021/js/script.js
+++ app/public/wp-content/themes/Wooms_2021/js/script.js
@@ -656,7 +656,14 @@ function sideNav(){
     document.querySelector('.sidebar2__top').classList.remove('active');
   }
 }
-
+function sideNav2(){
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+  if(scrollTop > (document.querySelector('.page-header').clientHeight / 1.3)){
+    document.querySelector('.sidebar2__top').classList.add('active');
+  }else{
+    document.querySelector('.sidebar2__top').classList.remove('active');
+  }
+}
 
 let sTop;
 let sBottom;
@@ -784,3 +791,36 @@ if(!MQ.matches && document.querySelector('.sp-horizontal-scroll__icon')){
   });
 }
 
+document.addEventListener('DOMContentLoaded', function () {
+  const targetElement = document.querySelector('.site-footer');
+  const targetElement2 = document.querySelector('.sidebar2__top');
+  
+  const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+          if (entry.isIntersecting) {
+              // 要素がスクロール範囲に入ったらクラスを追加
+              targetElement2.classList.add('active2');
+              sideNav2();
+          } else {
+              // 要素がスクロール範囲から出たらクラスを削除（オプション）
+              targetElement2.classList.remove('active2');
+          }
+      });
+  }, {
+      root: null, // ビューポートを基準にする
+      rootMargin: '0px', // マージンを追加しない
+      threshold: 0 // 要素が少しでも見えたら発火
+  });
+  let ticking = false;
+
+  window.addEventListener('scroll', () => {
+      if (!ticking) {
+          window.requestAnimationFrame(() => {
+              observer.observe(targetElement);
+              ticking = false;
+          });
+          ticking = true;
+      }
+  });
+  // 対象の要素を監視
+});
